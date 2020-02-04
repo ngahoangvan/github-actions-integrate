ed7e977aeb23cc91eaaa0dad2bd94948336ed4e0
Hoàng Văn Ngà <ngahv2222@gmail.com>
2020-02-04 11:13:56 +0700

    Config GitHub Action for PHP

--- php/run.php
+++ php/run.php
@@ -1,9 +1,9 @@
 <!-- Test Github Action -->
 <?php
 
-$txt1 = "Learn PHP";
+$txt1 = 'Learn PHP';
 $x = 5;
 $y = 4;
 
-echo "<h2>" . $txt1 . "</h2>";
-echo $x . " + " . $y . " = " . ($x + $y);
+echo '<h2>'.$txt1.'</h2>';
+echo $x.' + '.$y.' = '.($x + $y);
